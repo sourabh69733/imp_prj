4206406922b4985920ebf3d99b5d0c2a1446cb20
sourabh <sourabhsahu69733@gmail.com>
2021-07-07 11:25:33 +0530

    Added Router and Route

--- client-side/college_webApp/src/App.js
+++ client-side/college_webApp/src/App.js
@@ -1,35 +1,24 @@
 import React from 'react';
 import './styles.css';
-// import { BrowserRouter as Router, Route } from "react-router-dom";
 import WeeksComponent  from './components/weeks.component';
-import { ReadMoreButtton } from "./components/smallFunction";
+
 import DashboardInput from "./components/dashboard";
-import Assignments from './components/assignment';
-import Navbar from './components/navbar.component';
 import LessonsPlayer  from './components/vedioPlayer';
+
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Link,
-  useParams,
 } from "react-router-dom";
 
 function App() {
-  const hidenContent =(
-    <h5>
-      It is working. 
-    </h5>
-  )
-  const lessonIdArr = ["60dbffa46219d31f38445095"];
+  
   return (
     <div>
-      <WeeksComponent />
-      {/* <Navbar /> */}
       <br/>
       <Router>
-        
-        <Route path="/:id" component={LessonsPlayer}/>
+        <Route path="/" component={DashboardInput} />
+        <Route exact path="/weeks/" component={WeeksComponent}/>
+        <Route exact path="/:id" component={LessonsPlayer}/>
       </Router>
     </div>
   );
